0a1647c3fb58841f132996a1460112cff448eed7
YUKTHA LOKINENI <160403592+YUKTHALOKINENI@users.noreply.github.com>
2024-11-15 09:22:12 +0530

    Create season_game.py

---
+++ vissa_prep/season_game.py
@@ -0,0 +1,11 @@
+n=int(input())
+if n>=3 and n<=5:
+    print("Spring")
+elif n>=6 and n<=8:
+    print("Summer")
+elif n>=9 and n<=11:
+    print("Autumn")
+elif (n>=1 and n<=2) or n==12:
+    print("Winter")
+else:
+    print("Invalid")
