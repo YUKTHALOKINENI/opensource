386626447248b686869e88938427ebc6158dd2c3
YUKTHA LOKINENI <160403592+YUKTHALOKINENI@users.noreply.github.com>
2024-11-17 11:45:13 +0530

    Create DND.py

---
+++ vissa_prep/DND.py
@@ -0,0 +1,11 @@
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))
+num1=0
+num2=0
+for i in arr:
+    if i%m==0:
+        num2+=i
+    else:
+        num1+=i
+diference=num2-num1
+print(diference)
